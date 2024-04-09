a2a5253c9185f559df373d01d9a6e40cbe7bac8a
Nobob1905 <thanhcongzxc456@gmail.com>
2024-04-09 23:01:52 +0700

    Creat method sortByName and sortByAvg in StudentUtils

--- StudentUtils.java
+++ StudentUtils.java
@@ -24,4 +24,20 @@ public class StudentUtils {
         list.forEach(System.out::println); // example of Method Reference
         System.out.println("============ END ============ \n");
     }
+
+    public static void sortByName(List<Student> list) {
+        Collections.sort(list,  (Student o1, Student o2) -> {  //  <--- lambda expression
+                return o1.name.compareTo(o2.name);
+        });
+    }
+
+   
+    public static void sortByAvg(List<Student> list)
+    {
+        Collections.sort(list, (Student o1, Student o2) -> {
+                if (o1.average() > o2.average()) return 1;
+                if (o1.average() < o2.average()) return -1;
+                return 0;
+            });
+    }
 }
